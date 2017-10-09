c3afa787f65a78bf1220085f125ef07bb360a439
jeantimex <jean.timex@gmail.com>
2017-10-08 21:42:33 -0700

    Added tree problem.

---
+++ src/tree/binary-tree-inorder-traversal.js
@@ -0,0 +1,32 @@
+/**
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * For example:
+ * Given binary tree [1,null,2,3],
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ * return [1,3,2].
+ * 
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ */
+const inorderTraversal = root => {
+  const res = [];
+
+  const helper = (root, res) => {
+    if (!root) {
+      return;
+    }
+
+    helper(root.left, res);
+    res.push(root.val);
+    helper(root.right, res);
+  };
+
+  helper(root, res);
+  return res;
+};
+
+export default inorderTraversal;
