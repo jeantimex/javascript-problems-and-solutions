d7d73d1e7e694955a3edbc497e10e673e1064e68
jeantimex <jean.timex@gmail.com>
2017-10-08 22:22:29 -0700

    Added tree test cases.

--- src/utils/__tests__/tree-util-test.js
+++ src/utils/__tests__/tree-util-test.js
@@ -1,82 +1,17 @@
 import { assert } from 'chai';
 import TreeNode from '../../common/tree-node';
 import { serializeTree, deserializeTree } from '../tree-util';
-
-/**
- *        1
- *       / \
- *      2   3
- */
-const tree1 = () => {
-  const root = new TreeNode(1);
-  root.left = new TreeNode(2);
-  root.right = new TreeNode(3);
-  return root;
-};
-
-/**
- *        1
- *         \
- *          2
- */
-const tree2 = () => {
-  const root = new TreeNode(1);
-  root.right = new TreeNode(2);
-  return root;
-};
-
-/**
- *        1
- *         \
- *          2
- *         / \
- *        3   4
- */
-const tree3 = () => {
-  const root = new TreeNode(1);
-  root.right = new TreeNode(2);
-  root.right.left = new TreeNode(3);
-  root.right.right = new TreeNode(4);
-  return root;
-};
-
-/**
- *        1
- *         \
- *          2
- *         /
- *        3
- */
-const tree4 = () => {
-  const root = new TreeNode(1);
-  root.right = new TreeNode(2);
-  root.right.left = new TreeNode(3);
-  return root;
-};
-
-/**
- *        1
- *       /
- *      2
- *     /
- *    3
- */
-const tree5 = () => {
-  const root = new TreeNode(1);
-  root.left = new TreeNode(2);
-  root.left.left = new TreeNode(3);
-  return root;
-};
+import { tree1, tree2, tree3, tree4, tree5 } from '../../tree/__tests__/trees';
 
 describe('Tree Util', () => {
   describe('Serialize', () => {
     const testCases = [
       [null, null],
-      [tree1(), '[1,2,3]'],
-      [tree2(), '[1,null,2]'],
-      [tree3(), '[1,null,2,3,4]'],
-      [tree4(), '[1,null,2,3]'],
-      [tree5(), '[1,2,null,3]'],
+      [tree1, '[1,2,3]'],
+      [tree2, '[1,null,2]'],
+      [tree3, '[1,null,2,3,4]'],
+      [tree4, '[1,null,2,3]'],
+      [tree5, '[1,2,null,3]'],
     ];
 
     testCases.map((testCase, index) => {
