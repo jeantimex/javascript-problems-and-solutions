fdee93d96f43889ca875e4908aab6c14125e5150
jeantimex <jean.timex@gmail.com>
2017-10-12 11:32:24 -0700

    Added recover binary search tree problem.

---
+++ src/tree/__tests__/recover-binary-search-tree-test.js
@@ -0,0 +1,17 @@
+import { assert } from 'chai';
+import { serializeTree, deserializeTree } from '../../utils/tree-util';
+import recoverTree from '../recover-binary-search-tree';
+
+describe('Recover Binary Search Tree', () => {
+  const testCases = [['0,1', '1,0'], ['4,2,3,1,5', '4,2,5,1,3']];
+
+  testCases.map((testCase, index) => {
+    it(`should recover the binary search tree: ${testCase[0]}`, () => {
+      const root = deserializeTree(testCase[0]);
+      const expected = testCase[1];
+      recoverTree(root);
+      const actual = serializeTree(root);
+      assert.equal(actual, expected);
+    });
+  });
+});
