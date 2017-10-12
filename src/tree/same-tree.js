f014e0a589af2ff282b7af3b4c89a04b20500dc1
jeantimex <jean.timex@gmail.com>
2017-10-12 12:25:28 -0700

    Updated problem description.

--- src/tree/same-tree.js
+++ src/tree/same-tree.js
@@ -2,6 +2,12 @@
  * Given two binary trees, write a function to check if they are equal or not.
  * 
  * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+ * 
+ * Nothing fancy, just perform a preorder traversal for both trees at the same pace, simply return false whenever 
+ * we found a mismatch, otherwise continue the traversal on both left and right subtrees.
+ * 
+ * Time complexity: O(n)
+ * Space complexity: O(1) (ignore recursion stack, otherwise the height of the tree)
  */
 
 /**
