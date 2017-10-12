0bdfe68b74dbee0919748036888a8edf1ae408e9
jeantimex <jean.timex@gmail.com>
2017-10-12 14:57:03 -0700

    Added symmetric tree iterative solution.

--- src/tree/__tests__/symmetric-tree-test.js
+++ src/tree/__tests__/symmetric-tree-test.js
@@ -1,15 +1,33 @@
 import { assert } from 'chai';
 import { deserializeTree } from 'utils/tree-util';
-import { isSymmetric } from '../symmetric-tree';
+import { isSymmetric, isSymmetricDFS, isSymmetricBFS } from '../symmetric-tree';
 
 describe('Symmetric Tree', () => {
   const testCases = [['null', true], ['1,2,2', true], ['1,2,2,3,4,4,3', true], ['1,2,2,null,3,null,3', false]];
 
   testCases.map((testCase, index) => {
-    it(`should validate symmetric tree ${testCase[0]}`, () => {
+    it(`should validate symmetric tree recursively ${testCase[0]}`, () => {
       const root = deserializeTree(testCase[0]);
       const expected = testCase[1];
       const actual = isSymmetric(root);
+      assert.equal(actual, expected);
+    });
+  });
+
+  testCases.map((testCase, index) => {
+    it(`should validate symmetric tree using DFS iteratively ${testCase[0]}`, () => {
+      const root = deserializeTree(testCase[0]);
+      const expected = testCase[1];
+      const actual = isSymmetricDFS(root);
+      assert.equal(actual, expected);
+    });
+  });
+
+  testCases.map((testCase, index) => {
+    it(`should validate symmetric tree using BFS iteratively ${testCase[0]}`, () => {
+      const root = deserializeTree(testCase[0]);
+      const expected = testCase[1];
+      const actual = isSymmetricBFS(root);
       assert.equal(actual, expected);
     });
   });
