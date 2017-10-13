4e0ce0493b529ac6ea8d4db92b022627604f897e
jeantimex <jean.timex@gmail.com>
2017-10-12 20:50:22 -0700

    Added maximum depth of binary tree problem.

---
+++ src/tree/maximum-depth-of-binary-tree.js
@@ -0,0 +1,19 @@
+/**
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = root => {
+  if (!root) {
+    return 0;
+  }
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+export default maxDepth;
