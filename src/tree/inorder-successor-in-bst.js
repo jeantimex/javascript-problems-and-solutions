01ec46a9a0b03a51490715ac66675327d92eab0b
jeantimex <jean.timex@gmail.com>
2017-10-21 22:42:59 -0700

    Added inorder successor in bst.

---
+++ src/tree/inorder-successor-in-bst.js
@@ -0,0 +1,60 @@
+/**
+ * Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+ * 
+ * Note: If the given node has no in-order successor in the tree, return null.
+ */
+
+/**
+ * Recursion
+ * 
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+const inorderSuccessorR = (root, p) => {
+  if (!root) {
+    return null;
+  }
+
+  if (root.val <= p.val) {
+    return inorderSuccessorR(root.right, p);
+  }
+
+  const left = inorderSuccessorR(root.left, p);
+  return left ? left : root;
+};
+
+/**
+ * Iterative
+ * 
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+const inorderSuccessor = (root, p) => {
+  if (p.right) {
+    return getMinNode(p.right);
+  }
+
+  let successor = null;
+
+  while (root) {
+    if (p.val >= root.val) {
+      root = root.right;
+    } else {
+      successor = root;
+      root = root.left;
+    }
+  }
+
+  return successor;
+};
+
+const getMinNode = root => {
+  while (root.left) {
+    root = root.left;
+  }
+  return root;
+};
+
+export { inorderSuccessor, inorderSuccessorR };
