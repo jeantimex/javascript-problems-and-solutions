9e28b440931676668d1f89670acf225d55c9f9e8
jeantimex <jean.timex@gmail.com>
2017-10-24 21:00:23 -0700

    feat(tree): Added convert to bst to greater tree.

---
+++ src/tree/convert-bst-to-greater-tree.js
@@ -0,0 +1,46 @@
+/**
+ * Given a Binary Search Tree (BST), convert it to a Greater Tree such that
+ * every key of the original BST is changed to the original key plus sum of
+ * all keys greater than the original key in BST.
+ * 
+ * Example:
+ * 
+ * Input: The root of a Binary Search Tree like this:
+ *               5
+ *             /   \
+ *            2     13
+ * 
+ * Output: The root of a Greater Tree like this:
+ *              18
+ *             /   \
+ *           20     13
+ */
+
+import TreeNode from 'common/tree-node';
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const convertBST = root => {
+  let last = 0;
+
+  const helper = root => {
+    if (!root) {
+      return null;
+    }
+
+    helper(root.right);
+
+    root.val += last;
+    last = root.val;
+
+    helper(root.left);
+
+    return root;
+  };
+
+  return helper(root);
+};
+
+export default convertBST;
