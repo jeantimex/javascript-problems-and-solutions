7e553be1623cc99ea8e87e32ccfd8db1609a19e4
jeantimex <jean.timex@gmail.com>
2017-11-21 17:00:56 -0800

    feat(graph): Added BFS solution!

--- src/graph/sequence-reconstruction.js
+++ src/graph/sequence-reconstruction.js
@@ -1,46 +1,46 @@
 /**
- * Check whether the original sequence org can be uniquely reconstructed from the sequences in seqs. 
- * The org sequence is a permutation of the integers from 1 to n, with 1 ≤ n ≤ 104. Reconstruction 
- * means building a shortest common supersequence of the sequences in seqs (i.e., a shortest sequence 
- * so that all sequences in seqs are subsequences of it). Determine whether there is only one sequence 
+ * Check whether the original sequence org can be uniquely reconstructed from the sequences in seqs.
+ * The org sequence is a permutation of the integers from 1 to n, with 1 ≤ n ≤ 104. Reconstruction
+ * means building a shortest common supersequence of the sequences in seqs (i.e., a shortest sequence
+ * so that all sequences in seqs are subsequences of it). Determine whether there is only one sequence
  * that can be reconstructed from seqs and it is the org sequence.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input:
  * org: [1,2,3], seqs: [[1,2],[1,3]]
- * 
+ *
  * Output:
  * false
- * 
+ *
  * Explanation:
- * [1,2,3] is not the only one sequence that can be reconstructed, because [1,3,2] is also a valid 
+ * [1,2,3] is not the only one sequence that can be reconstructed, because [1,3,2] is also a valid
  * sequence that can be reconstructed.
  * Example 2:
- * 
+ *
  * Input:
  * org: [1,2,3], seqs: [[1,2]]
- * 
+ *
  * Output:
  * false
- * 
+ *
  * Explanation:
  * The reconstructed sequence can only be [1,2].
  * Example 3:
- * 
+ *
  * Input:
  * org: [1,2,3], seqs: [[1,2],[1,3],[2,3]]
- * 
+ *
  * Output:
  * true
- * 
+ *
  * Explanation:
  * The sequences [1,2], [1,3], and [2,3] can uniquely reconstruct the original sequence [1,2,3].
  * Example 4:
- * 
+ *
  * Input:
  * org: [4,1,5,2,6,3], seqs: [[5,2,6,3],[4,1,5,2]]
- * 
+ *
  * Output:
  * true
  */
@@ -61,12 +61,61 @@ export const sequenceReconstructionDFS = (org, seqs) => {
 
   const results = [];
 
-  allTopologicalSortDFS(adjList, indegree, visited, results, []);
+  dfs(adjList, indegree, visited, results, []);
 
   return results.length === 1 && results[0].toString() === org.toString();
 };
 
-const allTopologicalSortDFS = (adjList, indegree, visited, results, solution) => {
+/**
+ * BFS Solution
+ *
+ * @param {number[]} org
+ * @param {number[][]} seqs
+ * @return {boolean}
+ */
+export const sequenceReconstructionBFS = (org, seqs) => {
+  // Step 1. Build the graph using adjacency list and indgree map
+  const { adjList, indegree } = buildGraph(seqs);
+
+  // Step 2. BFS
+  const verticies = [...adjList.keys()];
+
+  if (verticies.length !== org.length) {
+    return false;
+  }
+
+  // Create a queue and enqueue all vertices with indegree 0
+  const queue = verticies.filter(u => indegree.get(u) === 0);
+
+  // Initialize count of visited vertices
+  let index = 0;
+
+  while (queue.length > 0) {
+    const size = queue.length;
+
+    if (size > 1) {
+      return false;
+    }
+
+    const u = queue.shift();
+
+    if (u !== org[index++]) {
+      return false;
+    }
+
+    adjList.get(u).forEach(v => {
+      indegree.set(v, indegree.get(v) - 1);
+
+      if (indegree.get(v) === 0) {
+        queue.push(v);
+      }
+    });
+  }
+
+  return index === org.length;
+};
+
+const dfs = (adjList, indegree, visited, results, solution) => {
   const verticies = [...adjList.keys()];
 
   // To indicate whether all topological are found or not
@@ -89,7 +138,7 @@ const allTopologicalSortDFS = (adjList, indegree, visited, results, solution) =>
       solution.push(u);
       visited.add(u);
 
-      allTopologicalSortDFS(adjList, indegree, visited, results, solution);
+      dfs(adjList, indegree, visited, results, solution);
 
       // Resetting visited, res and indegree for backtracking
       visited.delete(u);
