18d405c0a3bfdff8ccde0047d83750411074828d
jeantimex <jean.timex@gmail.com>
2018-06-03 23:53:10 -0700

    Fix test cases

--- src/dynamic-programming/__tests__/best-time-to-buy-and-sell-stock-ii-test.js
+++ src/dynamic-programming/__tests__/best-time-to-buy-and-sell-stock-ii-test.js
@@ -1,12 +1,17 @@
 import { assert } from 'chai';
-import { maxProfit } from '../best-time-to-buy-and-sell-stock-ii';
+import { maxProfit_I, maxProfit_II } from '../best-time-to-buy-and-sell-stock-ii';
 
 describe('Best Time to Buy and Sell Stock II', () => {
   const testCases = [[[7, 1, 5, 3, 6, 4], 7], [[1, 2, 3, 4, 5], 4], [[7, 6, 4, 3, 1], 0], [null, 0], [[], 0]];
 
   testCases.forEach(([prices, expected], index) => {
-    it(`should calculate the max profit, ${index}`, () => {
-      const actual = maxProfit(prices);
+    it(`should calculate the max profit I, ${index}`, () => {
+      const actual = maxProfit_I(prices);
+      assert.equal(actual, expected);
+    });
+
+    it(`should calculate the max profit II, ${index}`, () => {
+      const actual = maxProfit_II(prices);
       assert.equal(actual, expected);
     });
   });
