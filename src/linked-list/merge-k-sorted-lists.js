fabc1ac8810f0feacec0b82a2650e067007744bb
jeantimex <jean.timex@gmail.com>
2018-06-09 14:55:37 -0700

    Updated

--- src/linked-list/merge-k-sorted-lists.js
+++ src/linked-list/merge-k-sorted-lists.js
@@ -31,7 +31,54 @@
  * @return {ListNode}
  */
 const mergeKLists = lists => {
-  
+  if (!lists || lists.length === 0) {
+    return null;
+  }
+
+  const merge = (a, b) => {
+    if (!a || !b) {
+      return a || b;
+    }
+
+    let result;
+
+    if (a.val < b.val) {
+      result = a;
+      result.next = merge(a.next, b);
+    } else {
+      result = b;
+      result.next = merge(a, b.next);
+    }
+
+    return result;
+  };
+
+  const helper = (arr, last) => {
+    // repeat until only one list is left
+    while (last !== 0) {
+      let i = 0,
+        j = last;
+
+      // (i, j) forms a pair
+      while (i < j) {
+        // merge List i with List j and store
+        // merged list in List i
+        arr[i] = merge(arr[i], arr[j]);
+
+        // consider next pair
+        i++, j--;
+
+        // if all pairs are merged, update last
+        if (i >= j) {
+          last = j;
+        }
+      }
+    }
+
+    return lists[0];
+  };
+
+  return helper(lists, lists.length - 1);
 };
 
 export { mergeKLists };
