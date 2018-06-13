ad651ab770813eed7b9615a0c6683482d8284328
jeantimex <jean.timex@gmail.com>
2018-06-13 08:44:00 -0700

    Update next permutation

--- src/array/next-permutation.js
+++ src/array/next-permutation.js
@@ -18,51 +18,30 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const nextPermutation = nums => {
-  if (nums == null || nums.length == 0) {
-    return;
-  }
-
   const n = nums.length;
 
-  // step 1. scan from right and find the digit that is lower than the one on its right
-  let p = n - 1;
-  while (p > 0 && nums[p - 1] >= nums[p]) {
-    p--;
-  }
-
-  if (p == 0) {
-    // no such digit is found, the whole array is sorted in descending order
-    // we can simply reverse it
-    reverse(nums, 0, n - 1);
-    return;
-  }
-
-  // step 2. from p, find the digit that is just larger than nums[p - 1]
-  let i = p - 1;
-  let j = p;
-
-  while (p < n) {
-    if (nums[p] > nums[i] && nums[p] <= nums[j]) {
-      j = p;
+  // Step 1. scan from right and find the first digit that is less than its right
+  for (let i = n - 2; i >= 0; i--) {
+    if (nums[i] < nums[i + 1]) {
+      // Step 2. scan from right and find the digit that is larger than nums[i]
+      for (let j = n - 1; j > i; j--) {
+        if (nums[j] > nums[i]) {
+          // Step 3. swap nums[i] and nums[j], reverse from i+1
+          swap(nums, i, j);
+          reverse(nums, i + 1, n - 1);
+          return;
+        }
+      }
     }
-    p++;
   }
 
-  // step 3. swap i & j
-  swap(nums, i, j);
-
-  // step 4. reverse the digits after i
-  reverse(nums, i + 1, n - 1);
+  nums.reverse();
 };
 
-const swap = (nums, i, j) => {
-  const t = nums[i];
-  nums[i] = nums[j];
-  nums[j] = t;
-};
+const swap = (nums, i, j) => ([nums[i], nums[j]] = [nums[j], nums[i]]);
 
-const reverse = (nums, i, j) => {
-  while (i < j) {
-    swap(nums, i++, j--);
+const reverse = (nums, start, end) => {
+  while (start < end) {
+    swap(nums, start++, end--);
   }
 };
