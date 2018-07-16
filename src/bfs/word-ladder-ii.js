b116566b8672ba05f192486a459c271f6f8562f6
jeantimex <jean.timex@gmail.com>
2018-07-15 23:17:30 -0700

    BFS

--- src/bfs/word-ladder-ii.js
+++ src/bfs/word-ladder-ii.js
@@ -46,302 +46,63 @@
  * @param {string[]} wordList
  * @return {string[][]}
  */
-var findLadders = function(beginWord, endWord, wordList) {
+const findLadders = (beginWord, endWord, wordList) => {
+  const result = [];
   const dict = new Set(wordList);
-  const result = {
-    list: [],
-    min: wordList.length,
-  };
+  const queue = [[beginWord]];
+  const words = new Set();
 
-  backtracking(beginWord, endWord, dict, [beginWord], result);
+  let level = 1;
+  let minLevel = Number.MAX_SAFE_INTEGER;
 
-  return result.list;
-};
+  while (queue.length > 0) {
+    const path = queue.shift();
 
-const backtracking = (beginWord, endWord, dict, solution, result) => {
-  if (beginWord === endWord) {
-    const path = solution.slice();
-
-    if (path.length < result.min) {
-      result.list = [path];
-      result.min = path.length;
-    } else if (path.length === result.min) {
-      result.list.push(path);
-    }
-    return;
-  }
-
-  if (solution.length >= result.min) {
-    return;
-  }
-
-  const characters = beginWord.split('');
-
-  for (let i = 0; i < characters.length; i++) {
-    const char = characters[i];
-
-    for (let j = 0; j < 26; j++) {
-      characters[i] = String.fromCharCode(97 + j);
-      const newWord = characters.join('');
-
-      if (newWord === beginWord) {
-        continue;
+    // Clean up previous level
+    if (path.length > level) {
+      for (let [word] of words.entries()) {
+        dict.delete(word);
       }
 
-      if (dict.has(newWord)) {
-        dict.delete(newWord);
-        solution.push(newWord);
+      words.clear();
 
-        backtracking(newWord, endWord, dict, solution, result);
+      level = path.length;
 
-        solution.pop();
-        dict.add(newWord);
+      if (level > minLevel) {
+        break;
       }
     }
 
-    characters[i] = char;
+    const characters = path[path.length - 1].split('');
+
+    for (let i = 0; i < characters.length; i++) {
+      const char = characters[i];
+
+      for (let j = 0; j < 26; j++) {
+        characters[i] = String.fromCharCode(97 + j);
+        const newWord = characters.join('');
+
+        if (!dict.has(newWord)) {
+          continue;
+        }
+
+        words.add(newWord);
+        const newPath = path.slice();
+        newPath.push(newWord);
+
+        if (newWord === endWord) {
+          result.push(newPath);
+          minLevel = level;
+        } else {
+          queue.push(newPath);
+        }
+      }
+
+      characters[i] = char;
+    }
   }
+
+  return result;
 };
 
 export { findLadders };
-
-// const beginWord = 'hit';
-// const endWord = 'cog';
-// const wordList = ['hot', 'dot', 'dog', 'lot', 'log', 'cog'];
-
-// const beginWord = 'game';
-// const endWord = 'thee';
-// const wordList = ['frye', 'heat', 'tree', 'thee', 'game', 'free', 'hell', 'fame', 'faye'];
-
-const beginWord = 'magic';
-const endWord = 'pearl';
-const wordList = [
-  'flail',
-  'halon',
-  'lexus',
-  'joint',
-  'pears',
-  'slabs',
-  'lorie',
-  'lapse',
-  'wroth',
-  'yalow',
-  'swear',
-  'cavil',
-  'piety',
-  'yogis',
-  'dhaka',
-  'laxer',
-  'tatum',
-  'provo',
-  'truss',
-  'tends',
-  'deana',
-  'dried',
-  'hutch',
-  'basho',
-  'flyby',
-  'miler',
-  'fries',
-  'floes',
-  'lingo',
-  'wider',
-  'scary',
-  'marks',
-  'perry',
-  'igloo',
-  'melts',
-  'lanny',
-  'satan',
-  'foamy',
-  'perks',
-  'denim',
-  'plugs',
-  'cloak',
-  'cyril',
-  'women',
-  'issue',
-  'rocky',
-  'marry',
-  'trash',
-  'merry',
-  'topic',
-  'hicks',
-  'dicky',
-  'prado',
-  'casio',
-  'lapel',
-  'diane',
-  'serer',
-  'paige',
-  'parry',
-  'elope',
-  'balds',
-  'dated',
-  'copra',
-  'earth',
-  'marty',
-  'slake',
-  'balms',
-  'daryl',
-  'loves',
-  'civet',
-  'sweat',
-  'daley',
-  'touch',
-  'maria',
-  'dacca',
-  'muggy',
-  'chore',
-  'felix',
-  'ogled',
-  'acids',
-  'terse',
-  'cults',
-  'darla',
-  'snubs',
-  'boats',
-  'recta',
-  'cohan',
-  'purse',
-  'joist',
-  'grosz',
-  'sheri',
-  'steam',
-  'manic',
-  'luisa',
-  'gluts',
-  'spits',
-  'boxer',
-  'abner',
-  'cooke',
-  'scowl',
-  'kenya',
-  'hasps',
-  'roger',
-  'edwin',
-  'black',
-  'terns',
-  'folks',
-  'demur',
-  'dingo',
-  'party',
-  'brian',
-  'numbs',
-  'forgo',
-  'gunny',
-  'waled',
-  'bucks',
-  'titan',
-  'ruffs',
-  'pizza',
-  'ravel',
-  'poole',
-  'suits',
-  'stoic',
-  'segre',
-  'white',
-  'lemur',
-  'belts',
-  'scums',
-  'parks',
-  'gusts',
-  'ozark',
-  'umped',
-  'heard',
-  'lorna',
-  'emile',
-  'orbit',
-  'onset',
-  'cruet',
-  'amiss',
-  'fumed',
-  'gelds',
-  'italy',
-  'rakes',
-  'loxed',
-  'kilts',
-  'mania',
-  'tombs',
-  'gaped',
-  'merge',
-  'molar',
-  'smith',
-  'tangs',
-  'misty',
-  'wefts',
-  'yawns',
-  'smile',
-  'scuff',
-  'width',
-  'paris',
-  'coded',
-  'sodom',
-  'shits',
-  'benny',
-  'pudgy',
-  'mayer',
-  'peary',
-  'curve',
-  'tulsa',
-  'ramos',
-  'thick',
-  'dogie',
-  'gourd',
-  'strop',
-  'ahmad',
-  'clove',
-  'tract',
-  'calyx',
-  'maris',
-  'wants',
-  'lipid',
-  'pearl',
-  'maybe',
-  'banjo',
-  'south',
-  'blend',
-  'diana',
-  'lanai',
-  'waged',
-  'shari',
-  'magic',
-  'duchy',
-  'decca',
-  'wried',
-  'maine',
-  'nutty',
-  'turns',
-  'satyr',
-  'holds',
-  'finks',
-  'twits',
-  'peaks',
-  'teems',
-  'peace',
-  'melon',
-  'czars',
-  'robby',
-  'tabby',
-  'shove',
-  'minty',
-  'marta',
-  'dregs',
-  'lacks',
-  'casts',
-  'aruba',
-  'stall',
-  'nurse',
-  'jewry',
-  'knuth',
-];
-
-console.log(findLadders(beginWord, endWord, wordList));
-
-// console.log([
-//   ['magic', 'manic', 'mania', 'maria', 'marta', 'marty', 'party', 'parry', 'perry', 'peary', 'pearl'],
-//   ['magic', 'manic', 'mania', 'maria', 'maris', 'paris', 'parks', 'perks', 'peaks', 'pears', 'pearl'],
-//   ['magic', 'manic', 'mania', 'maria', 'marta', 'marty', 'marry', 'merry', 'perry', 'peary', 'pearl'],
-//   ['magic', 'manic', 'mania', 'maria', 'marta', 'marty', 'marry', 'parry', 'perry', 'peary', 'pearl'],
-//   ['magic', 'manic', 'mania', 'maria', 'maris', 'marks', 'parks', 'perks', 'peaks', 'pears', 'pearl'],
-// ]);
