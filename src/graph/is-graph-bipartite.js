d757314bb6466bd99c81a6cb3c3f8495ce6ce220
jeantimex <jean.timex@gmail.com>
2018-08-11 14:36:02 -0700

    Fix the function name

--- src/graph/is-graph-bipartite.js
+++ src/graph/is-graph-bipartite.js
@@ -90,7 +90,7 @@ const dfs = (graph, colors, u, color) => {
  * @param {number[][]} graph
  * @return {boolean}
  */
-const isBipartiteBFS = graph => {
+const isBipartite_BFS = graph => {
   const colors = new Map();
 
   for (let u = 0; u < graph.length; u++) {
