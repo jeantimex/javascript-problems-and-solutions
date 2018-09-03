6129dd7114c8b94ef38bec9e740f9bdc40d6bd1c
jeantimex <jean.timex@gmail.com>
2018-09-03 14:26:17 -0700

    Linked List Cycle

---
+++ src/linked-list/linked-list-cycle.js
@@ -0,0 +1,38 @@
+/**
+ * Linked List Cycle
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * Follow up:
+ * Can you solve it without using extra space?
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = head => {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+export { hasCycle };
