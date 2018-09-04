47320f405666cbc6a3f1b2f5c2c04a63f3b40f5f
jeantimex <jean.timex@gmail.com>
2018-09-04 09:52:09 -0700

    Intersection of Two Linked Lists

---
+++ src/linked-list/intersection-of-two-linked-lists.js
@@ -0,0 +1,52 @@
+/**
+ * Intersection of Two Linked Lists
+ *
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ *
+ * For example, the following two linked lists:
+ *
+ * A:          a1 → a2
+ *                    ↘
+ *                      c1 → c2 → c3
+ *                    ↗
+ * B:     b1 → b2 → b3
+ * begin to intersect at node c1.
+ *
+ *
+ * Notes:
+ *
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function returns.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+const getIntersectionNode = (headA, headB) => {
+  if (!headA || !headB) {
+    return null;
+  }
+
+  let a = headA;
+  let b = headB;
+
+  while (a !== b) {
+    a = a ? a.next : headB;
+    b = b ? b.next : headA;
+  }
+
+  return a;
+};
+
+export { getIntersectionNode };
