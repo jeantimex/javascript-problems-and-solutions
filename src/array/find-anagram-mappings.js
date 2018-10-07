4e12ca424d2135f52fcd7f16bb3593d30a8c26f0
Su <jean.timex@gmail.com>
2018-10-07 15:03:25 -0700

    Update find-anagram-mappings.js

--- src/array/find-anagram-mappings.js
+++ src/array/find-anagram-mappings.js
@@ -1,7 +1,7 @@
 /**
  * Find Anagram Mappings
  *
- * Given two lists Aand B, and B is an anagram of A.
+ * Given two lists A and B, and B is an anagram of A.
  * B is an anagram of A means B is made by randomizing the order of the elements in A.
  *
  * We want to find an index mapping P, from A to B. A mapping P[i] = j means the ith element in A appears in B at index j.
