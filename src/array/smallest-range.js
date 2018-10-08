f026e843e69a2478c226170ab6a06cf7264d620d
jeantimex <jean.timex@gmail.com>
2018-10-07 23:38:13 -0700

    Add smallest range

---
+++ src/array/smallest-range.js
@@ -0,0 +1,26 @@
+/**
+ * Smallest Range
+ *
+ * You have k lists of sorted integers in ascending order. Find the smallest range that includes
+ * at least one number from each of the k lists.
+ *
+ * We define the range [a,b] is smaller than range [c,d] if b-a < d-c or a < c if b-a == d-c.
+ *
+ * Example 1:
+ *
+ * Input:[[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
+ * Output: [20,24]
+ *
+ * Explanation:
+ * List 1: [4, 10, 15, 24,26], 24 is in range [20,24].
+ * List 2: [0, 9, 12, 20], 20 is in range [20,24].
+ * List 3: [5, 18, 22, 30], 22 is in range [20,24].
+ */
+
+/**
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+const smallestRange = nums => {};
+
+export { smallestRange };
