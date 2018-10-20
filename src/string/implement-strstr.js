66506332dfcf3a25f0ec6064fed2906e18b3d8b8
Su <jean.timex@gmail.com>
2018-10-20 11:30:09 -0700

    Update implement-strstr.js

--- src/string/implement-strstr.js
+++ src/string/implement-strstr.js
@@ -1,14 +1,13 @@
 /**
  * Implement strStr()
  *
- * Implement strStr().
- *
  * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
  *
  * Example 1:
  *
  * Input: haystack = "hello", needle = "ll"
  * Output: 2
+ *
  * Example 2:
  *
  * Input: haystack = "aaaaa", needle = "bba"
