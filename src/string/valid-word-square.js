e90bce4df92f068e8d34e6b88e8e6b6d915ead40
Su <jean.timex@gmail.com>
2018-10-21 12:13:04 -0700

    Update valid-word-square.js

--- src/string/valid-word-square.js
+++ src/string/valid-word-square.js
@@ -79,6 +79,8 @@
 const validWordSquare = words => {
   for (let i = 0; i < words.length; i++) {
     for (let j = 0; j < words[i].length; j++) {
+      // (j >= words.length) means the the number of rows is too small
+      // (i >= words[j].length) means the word at j-th row is too short
       if (j >= words.length || i >= words[j].length || words[i][j] !== words[j][i]) {
         return false;
       }
