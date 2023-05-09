8ee2543131dcbb61607c65646e9b60e495334a5d
Ala Eddine <ala.official.acc@gmail.com>
2023-05-09 22:07:03 +0100

    :cyclone: Implement the brute force solution

--- src/array/two-sum.js
+++ src/array/two-sum.js
@@ -34,4 +34,29 @@ const twoSum = (nums, target) => {
   return null;
 };
 
+/**
+ * Brute Force
+ */
+
+/**
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+/**
+ * 
+ */
+const twoSumBrute = function (nums, target) {
+	for (let i = 0; i < nums.length - 1; i++) {
+		for (let nextIndex = i + 1; nextIndex < nums.length; nextIndex++) {
+			if (target === nums[i] + nums[nextIndex]) {
+				return [i, nextIndex];
+			}
+		}
+	}
+};
+
+console.log(twoSumBrute([3, 2, 4], 6)); // [0,1]
+ 
 export { twoSum };
