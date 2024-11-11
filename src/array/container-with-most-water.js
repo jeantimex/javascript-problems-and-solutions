9014362ee05e42d8a9c5e6f4ae93750ad29faa26
cassiano <cgrsobral@gmail.com>
2024-11-11 14:47:05 -0300

    mudei branch

--- src/array/container-with-most-water.js
+++ src/array/container-with-most-water.js
@@ -48,26 +48,3 @@
  * 6 x x x x x x
  */
 
-/**
- * @param {number[]} height
- * @return {number}
- */
-const maxArea = height => {
-  const n = height.length;
-  let max = 0;
-
-  for (let i = 0, j = n - 1; i < j; ) {
-    const area = Math.min(height[i], height[j]) * (j - i);
-    max = Math.max(max, area);
-
-    if (height[i] < height[j]) {
-      i++;
-    } else {
-      j--;
-    }
-  }
-
-  return max;
-};
-
-export { maxArea };
