5620334ce648070ddfea8ada799321a37524d60f
cassi35 <cgrsobral@gmail.com>
2025-01-30 11:03:03 -0300

    fiz coisas

--- src/matrix/brick-wall.js
+++ src/matrix/brick-wall.js
@@ -26,23 +26,4 @@
  * Output: 2
  */
 
-/**
- * @param {number[][]} wall
- * @return {number}
- */
-const leastBricks = wall => {
-  const map = {};
-  let count = 0;
 
-  for (let i = 0; i < wall.length; i++) {
-    for (let j = 0, sum = 0; j < wall[i].length - 1; j++) {
-      sum += wall[i][j];
-      map[sum] = ~~map[sum] + 1;
-      count = Math.max(count, map[sum]);
-    }
-  }
-
-  return wall.length - count;
-};
-
-export { leastBricks };
